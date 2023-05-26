32349303c63f5e7d67254e44d266c56a251b5e19
Kacper Kalinski <kacper.kalinski@miquido.com>
2023-05-26 07:24:13 +0000

    [MOB-1309] Fix Base32 decoding

--- Passbolt/PassboltPackage/Sources/Commons/Data+Base32.swift
+++ Passbolt/PassboltPackage/Sources/Commons/Data+Base32.swift
@@ -28,119 +28,98 @@ extension Data {
   public init?(
     base32Encoded string: String
   ) {
-    // convert string to bytes using ASCII
-    guard let stringData: Data = string.uppercased().data(using: .ascii, allowLossyConversion: false)
-    else {
-      return nil
-    }
-
-    // prepare input buffer
-    let inputBuffer: UnsafeMutableRawBufferPointer = .allocate(
-      // allocating exact or more than needed
-      byteCount: stringData.count,
-      alignment: 0
-    )
-    defer { inputBuffer.deallocate() }
-    inputBuffer
-      .initializeMemory(
-        as: UInt8.self,
-        repeating: 0
+    let data: Data? = string.utf8CString.withUnsafeBytes { (buffer: UnsafeRawBufferPointer) -> Data? in
+      // utf8CString is null terminated, actual data is 1 element less
+      let inputBufferSize: Int = buffer.count - 1
+      let outputBuffer: UnsafeMutableRawBufferPointer = .allocate(
+        // allocating rounding up, empty bytes will be skipped at the end
+        byteCount: inputBufferSize + inputBufferSize % 5,
+        alignment: 0
       )
-
-    var inputBytesCount: Int = 0
-    // iterate over string bytes and convert bytes
-    // according to conversion table, fail on invalid
-    // characters and count bytes for processing
-    for (offset, element): (Int, UInt8) in stringData.enumerated() {
-      // if value exists in table use it in input buffer
-      if let code: UInt8 = base32DecodingTable[element] {
-        inputBuffer[offset] = code
-        inputBytesCount += 1
-      }
-      else if element == paddingByte {
-        break  // skip padding, ends data
-      }
-      else {
-        return nil  // invalid characters
-      }
-    }
-
-    // prepare blocks count - number of octets (rounded up)
-    // to be converted to output
-    let blocksCount: Int = inputBytesCount / 8 + (inputBytesCount % 8 == 0 ? 0 : 1)
-
-    // prepare output buffer
-    let outputBufferBytesCount: Int = blocksCount * 5
-    let outputBuffer: UnsafeMutableRawBufferPointer = .allocate(
-      // allocating rounding up, empty bytes will be removed at the end
-      byteCount: Swift.max(outputBufferBytesCount, 5),
-      alignment: 0
-    )
-    outputBuffer
-      .initializeMemory(
-        as: UInt8.self,
-        repeating: 0
-      )
-    guard let outputAddress: UnsafeMutableRawPointer = outputBuffer.baseAddress
-    else { return nil }
-
-    // prepare buffer for converting octets in blocks
-    let blockBuffer: UnsafeMutableRawBufferPointer = .allocate(
-      byteCount: 8,
-      alignment: 0
-    )
-    defer { blockBuffer.deallocate() }
-
-    for block: Int in 0 ..< blocksCount {
-      // clear block buffer for each block
-      blockBuffer
+      outputBuffer
         .initializeMemory(
           as: UInt8.self,
           repeating: 0
         )
+      defer { outputBuffer.deallocate() }
 
-      let inputOffset: Int = block * 8
-      // count number of bytes available in block,
-      // it is 8 (octet) except possible reminder
-      let blockBytesCount: Int = (inputOffset + 8 > inputBytesCount) ? inputBytesCount % 8 : 8
+      guard let outputAddress: UnsafeMutableRawPointer = outputBuffer.baseAddress
+      else { return nil }
 
-      // write bytes from input to block buffer
-      for byteIndex: Int in 0 ..< blockBytesCount {
-        blockBuffer[byteIndex] = inputBuffer[inputOffset + byteIndex]
+      var outputBufferSize: Int = 0
+      var index: Int = 0
+      while index < inputBufferSize {
+        if let code: UInt8 = base32DecodingTable[buffer[index]] {
+          let outputIndex: Int = index * 5 / 8
+          switch index % 8 {
+          case 0:
+            outputBuffer[outputIndex] = (code << 3) & 0b11111000
+            // unfinished octets does not count to output
+
+          case 1:
+            outputBuffer[outputIndex] = outputBuffer[outputIndex] | ((code >> 2) & 0b00000111)
+            outputBuffer[outputIndex + 1] = (code << 6) & 0b11000000
+            outputBufferSize += 1
+
+          case 2:
+            outputBuffer[outputIndex] = outputBuffer[outputIndex] | ((code << 1) & 0b00111110)
+            // unfinished octets does not count to output
+
+          case 3:
+            outputBuffer[outputIndex] = outputBuffer[outputIndex] | ((code >> 4) & 0b00000001)
+            outputBuffer[outputIndex + 1] = (code << 4) & 0b11110000
+            outputBufferSize += 1
+
+          case 4:
+            outputBuffer[outputIndex] = outputBuffer[outputIndex] | ((code >> 1) & 0b00001111)
+            outputBuffer[outputIndex + 1] = (code << 7) & 0b10000000
+            outputBufferSize += 1
+
+          case 5:
+            outputBuffer[outputIndex] = outputBuffer[outputIndex] | ((code << 2) & 0b01111100)
+            // unfinished octets does not count to output
+
+          case 6:
+            outputBuffer[outputIndex] = outputBuffer[outputIndex] | ((code >> 3) & 0b00000011)
+            outputBuffer[outputIndex + 1] = (code << 5) & 0b11100000
+            outputBufferSize += 1
+
+          case 7:
+            outputBuffer[outputIndex] = outputBuffer[outputIndex] | (code & 0b00011111)
+            outputBufferSize += 1
+
+          case _:
+            unreachable("% 8 can't exceed 7")
+          }
+          index += 1
+        }
+        else if buffer[index] == paddingByte {
+          break  // padding ends data
+        }
+        else {
+          return nil  // invalid characters
+        }
       }
 
-      // write bytes to output buffer
-      let outputOffset: Int = block * 5
-      outputBuffer[outputOffset + 0] = ((blockBuffer[0] << 3) & 0xF8) | ((blockBuffer[1] >> 2) & 0x07)
-      outputBuffer[outputOffset + 1] =
-        ((blockBuffer[1] << 6) & 0xC0) | ((blockBuffer[2] << 1) & 0x3E) | ((blockBuffer[3] >> 4) & 0x01)
-      outputBuffer[outputOffset + 2] = ((blockBuffer[3] << 4) & 0xF0) | ((blockBuffer[4] >> 1) & 0x0F)
-      outputBuffer[outputOffset + 3] =
-        ((blockBuffer[4] << 7) & 0x80) | ((blockBuffer[5] << 2) & 0x7C) | ((blockBuffer[6] >> 3) & 0x03)
-      outputBuffer[outputOffset + 4] = ((blockBuffer[6] << 5) & 0xE0) | (blockBuffer[7] & 0x1F)
+      return .init(
+        bytes: outputAddress,
+        count: outputBufferSize
+      )
     }
 
-    // convert buffer to Data
-    var resultData: Data = .init(
-      bytesNoCopy: outputAddress,
-      count: outputBufferBytesCount,
-      deallocator: .free
-    )
-
-    // remove trailing zeros
-    while resultData.last == 0 {
-      resultData.removeLast()
+    if let data {
+      self = data
     }
-
-    // finalize init
-    self = resultData
+    else {
+      return nil
+    }
   }
 }
 
 // 61 is ASCII code for `=` which is used for padding
 private let paddingByte: UInt8 = 61
 private let base32DecodingTable: Dictionary<UInt8, UInt8> = [
-  // ASCII codes -> values
+  // ASCII codes (uppercased and lowercased) -> values
   65: 0, 66: 1, 67: 2, 68: 3,
   69: 4, 70: 5, 71: 6, 72: 7,
   73: 8, 74: 9, 75: 10, 76: 11,
@@ -149,4 +128,11 @@ private let base32DecodingTable: Dictionary<UInt8, UInt8> = [
   85: 20, 86: 21, 87: 22, 88: 23,
   89: 24, 90: 25, 50: 26, 51: 27,
   52: 28, 53: 29, 54: 30, 55: 31,
+  97: 0, 98: 1, 99: 2, 100: 3,
+  101: 4, 102: 5, 103: 6, 104: 7,
+  105: 8, 106: 9, 107: 10, 108: 11,
+  109: 12, 110: 13, 111: 14, 112: 15,
+  113: 16, 114: 17, 115: 18, 116: 19,
+  117: 20, 118: 21, 119: 22, 120: 23,
+  121: 24, 122: 25,
 ]
