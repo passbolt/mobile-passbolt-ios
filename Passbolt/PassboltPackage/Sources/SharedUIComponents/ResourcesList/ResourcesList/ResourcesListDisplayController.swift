73a375af0e4a8a0aa2bd2a8074386b72c9e603c9
Marcin Dziennik <marcin.dziennik@miquido.com>
2026-01-13 14:59:59 +0100

    MOB-3270 Resource context menu should not appear on Autofill extension

--- Passbolt/PassboltPackage/Sources/SharedUIComponents/ResourcesList/ResourcesList/ResourcesListDisplayController.swift
+++ Passbolt/PassboltPackage/Sources/SharedUIComponents/ResourcesList/ResourcesList/ResourcesListDisplayController.swift
@@ -138,9 +138,7 @@ extension ResourcesListDisplayController {
     try await self.context.selectResource(id)
   }
 
-  internal final func showResourceMenu(
-    _ id: Resource.ID
-  ) async throws {
-    try await self.context.resourceMenuAction?(id)
+  internal var showResourceMenuCallback: ((Resource.ID) async throws -> Void)? {
+    self.context.resourceMenuAction
   }
 }
